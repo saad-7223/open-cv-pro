16b509c3a5c25e94c2e0e405965058964a2ad391
saad-7223 <saadsyed837@gmail.com>
2023-08-30 08:52:54 +0530

    learning to capture video frams of a video

--- read.py
+++ read.py
@@ -1,10 +1,21 @@
 import cv2 as cv
 
 # to read images
-img = cv.imread("photos/cat_large.jpg")
+#img = cv.imread("photos/cat_large.jpg")
 
 # to display images
-cv.imshow('c',img)
+#cv.imshow('c',img)
 
+# to read videos
+capture = cv.VideoCapture("videos/billy.mp4")
+
+while True:
+    isTrue , frame = capture.read()
+    cv.imshow('v', frame)
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
 #this line acts as an on click closeing of the window
-cv.waitKey(0)
+
+capture.release()
+cv.destroyAllWindows()
